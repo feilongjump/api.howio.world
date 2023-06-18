3ced37a13fa61aef804dd5d589c35c71f7ae2930
ifeilong <feilongjump@qq.com>
2023-06-19 06:26:00 +0800

    :fire: Remove mews/purifier

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -3,8 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Support\Str;
-use Mews\Purifier\Facades\Purifier;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Content extends Model
 {
@@ -25,13 +23,6 @@ class Content extends Model
     protected $hidden = [
         'created_at', 'updated_at', 'deleted_at',
     ];
-
-    protected function body(): Attribute
-    {
-        return Attribute::make(
-            set: fn (string $value) => Purifier::clean($value),
-        );
-    }
 
     public static function toHTML(string $markdown): string
     {
