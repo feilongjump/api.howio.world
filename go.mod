cd1489a1d4e0ee600d1d72123836303b7e3e70e5
ifeilong <feilongjump@qq.com>
2023-10-08 08:03:09 +0800

    jwt

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/spf13/viper v1.16.0
 	golang.org/x/crypto v0.13.0
 	gorm.io/driver/mysql v1.5.1
---
+++ internal/jwt/jwt.go
@@ -0,0 +1,41 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/feilongjump/api.howio.world/internal/config"
+	"github.com/feilongjump/api.howio.world/internal/utils"
+	jwtpkg "github.com/golang-jwt/jwt/v5"
+)
+
+// 自定义载荷
+type JWTCustomClaims struct {
+	UserID uint64 `json:"user_id"`
+
+	jwtpkg.RegisteredClaims
+}
+
+// 生成 jwt 令牌
+func GenerateToken(UserID uint64) (string, error) {
+
+	appName := config.GetString("app.name")
+	expiresAt := time.Now().Add(time.Second * time.Duration(config.GetInt("jwt.ttl")))
+
+	claims := JWTCustomClaims{
+		UserID,
+		jwtpkg.RegisteredClaims{
+			Issuer:    appName,                                            // 签发者
+			Subject:   "API Token",                                        // 签发主题
+			Audience:  jwtpkg.ClaimStrings{appName + "_APP"},              // 签发受众
+			ExpiresAt: jwtpkg.NewNumericDate(expiresAt),                   // 过期时间
+			NotBefore: jwtpkg.NewNumericDate(time.Now().Add(time.Second)), // 最早使用时间
+			IssuedAt:  jwtpkg.NewNumericDate(time.Now()),                  // 签发时间
+			ID:        utils.GenerateRandomString(12),                     // wt ID, 类似于盐值
+		},
+	}
+
+	key := []byte(config.GetString("jwt.secret"))
+	token, err := jwtpkg.NewWithClaims(jwtpkg.SigningMethodHS256, claims).SignedString(key)
+
+	return token, err
+}
