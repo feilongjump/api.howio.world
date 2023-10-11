e008648e4665d79d7730dd17545d165ff15580f5
ifeilong <feilongjump@qq.com>
2023-10-11 11:47:40 +0800

    优化 Auth 中间件 & 新增 Guest 中间件

--- app/http/middlewares/auth.go
+++ app/http/middlewares/auth.go
@@ -1,43 +1,78 @@
 package middlewares
 
 import (
-	"strings"
-	"time"
-
 	"github.com/feilongjump/api.howio.world/internal/jwt"
 	"github.com/feilongjump/api.howio.world/internal/response"
 	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
 )
 
+type AuthUser struct {
+	UserID uint64
+}
+
+// Auth 用户登录
 func Auth() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		token := ctx.GetHeader("Authorization")
-		if token == "" {
-			response.Unauthorized(ctx)
+		authUser := AuthUser{}
+
+		if ok := checkAuthToken(ctx, &authUser, false); !ok {
 			return
 		}
 
-		tokenSplit := strings.Fields(token)
-		// 令牌类型是否正确
-		if tokenSplit[0] != "Bearer" {
-			response.Unauthorized(ctx)
-			return
-		}
-
-		// 令牌可否正常解析
-		claims, err := jwt.ParseToken(tokenSplit[1])
-		if err != nil {
-			response.Unauthorized(ctx)
-			return
-		}
-
-		// 令牌是否已过期
-		if claims.ExpiresAt.Unix() <= time.Now().Unix() {
-			response.Unauthorized(ctx)
-			return
-		}
-
-		ctx.Set("user_id", claims.UserID)
-		ctx.Next()
+		ctx.Set("user_id", authUser.UserID)
 	}
+}
+
+// Guest 游客登陆，验证是否存在用户 ID
+func Guest() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authUser := AuthUser{}
+
+		checkAuthToken(ctx, &authUser, false)
+
+		ctx.Set("user_id", authUser.UserID)
+	}
+}
+
+// CheckAuthToken 检查用户令牌
+func checkAuthToken(ctx *gin.Context, authUser *AuthUser, isAbort bool) bool {
+
+	token := ctx.GetHeader("Authorization")
+	if token == "" {
+		if isAbort {
+			response.Unauthorized(ctx)
+		}
+		return false
+	}
+
+	tokenSplit := strings.Fields(token)
+	// 令牌类型是否正确
+	if tokenSplit[0] != "Bearer" {
+		if isAbort {
+			response.Unauthorized(ctx)
+		}
+		return false
+	}
+
+	// 令牌可否正常解析
+	claims, err := jwt.ParseToken(tokenSplit[1])
+	if err != nil {
+		if isAbort {
+			response.Unauthorized(ctx)
+		}
+		return false
+	}
+
+	// 令牌是否已过期
+	if claims.ExpiresAt.Unix() <= time.Now().Unix() {
+		if isAbort {
+			response.Unauthorized(ctx)
+		}
+		return false
+	}
+
+	authUser.UserID = claims.UserID
+	return true
 }
