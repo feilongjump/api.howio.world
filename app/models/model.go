85dfa07005f545452ef940cfa597870bde29de48
ifeilong <feilongjump@qq.com>
2023-10-12 18:42:02 +0800

    model 分页器

--- app/models/model.go
+++ app/models/model.go
@@ -20,3 +20,20 @@ type BaseDeleteTimeModel struct {
 	DeletedAt gorm.DeletedAt `json:"-"`
 }
 
+// Paginator 分页器
+func Paginator(ctx *gin.Context) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		page, _ := strconv.Atoi(ctx.Query("page"))
+		if page <= 1 {
+			page = 1
+		}
+
+		perPage, _ := strconv.Atoi(ctx.Query("per_page"))
+		if perPage <= 0 {
+			perPage = 20
+		}
+
+		offset := (page - 1) * perPage
+		return db.Offset(offset).Limit(perPage)
+	}
+}
